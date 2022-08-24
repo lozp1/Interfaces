f8088b764413e61bf68ec991ab89865daf5de568
franco <91517547+lozp1@users.noreply.github.com>
2022-08-23 22:18:17 -0600

    Add files via upload

---
+++ 06.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php';
+
+interface TransporteInterface{
+    public function getInfo() : string;
+    public function getRuedas() : int; 
+}
+class Transporte implements TransporteInterface {
+    public function __construct(protected int $ruedas, protected int $capacidad){
+    }
+
+    public function getInfo(): string {
+        return "El transporte tiene: " . $this->ruedas . " y una capacidad de: " . $this->capacidad . " personas";
+    }
+
+    public function getRuedas() : int {
+        return $this -> ruedas;
+    }
+}
+
+include 'includes/footer.php';
